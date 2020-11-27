0788f2515090500dee45a951226981fa26385ff5
Bricx Carasco <bricxraincarasco21@gmail.com>
2020-11-27 22:30:10 +0800

    Practicing layout options in react native

--- rn-starter/src/screens/BoxScreen.js
+++ rn-starter/src/screens/BoxScreen.js
@@ -3,22 +3,40 @@ import { StyleSheet, View, Text } from "react-native";
 
 const BoxScreen = () => {
   return (
-    <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Box screen</Text>
+    <View style={styles.parentViewStyle}>
+      <View style={styles.viewStyle1} />
+      <View style={styles.viewStyle2} />
+      <View style={styles.viewStyle3} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  viewStyle: {
-    borderWidth: 3,
-    borderColor: "black",
+  parentViewStyle: {
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
-  textStyle: {
-    borderWidth: 5,
+  viewStyle1: {
+    borderWidth: 3,
     borderColor: "red",
-    margin: 20,
-    padding: 10,
+    backgroundColor: "red",
+    height: 50,
+    width: 50,
+  },
+  viewStyle2: {
+    borderWidth: 3,
+    borderColor: "green",
+    backgroundColor: "green",
+    height: 50,
+    width: 50,
+    top: 50,
+  },
+  viewStyle3: {
+    borderWidth: 3,
+    borderColor: "purple",
+    backgroundColor: "purple",
+    height: 50,
+    width: 50,
   },
 });
 
