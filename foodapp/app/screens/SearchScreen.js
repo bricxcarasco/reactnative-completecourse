92d9ed59223008dbfcb776b95702e43e515712ff
Bricx Carasco <bricxraincarasco21@gmail.com>
2020-11-30 15:16:35 +0800

    Setup axios and do yelp search businesses api

--- foodapp/app/screens/SearchScreen.js
+++ foodapp/app/screens/SearchScreen.js
@@ -2,18 +2,35 @@ import React, { useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import RestaurantList from "../components/RestaurantList";
 
+import yelp from "../api/yelp";
+
 import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
   const [term, onTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const onTermSubmit = () => {
-    console.log(term);
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term,
+          location: "san jose",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage("Something went wrong!");
+    }
   };
 
   return (
     <View>
-      <SearchBar term={term} onTerm={onTerm} onTermSubmit={onTermSubmit} />
+      <SearchBar term={term} onTerm={onTerm} onTermSubmit={searchApi} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results.</Text>
       <RestaurantList />
     </View>
   );
